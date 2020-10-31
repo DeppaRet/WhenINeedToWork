126869e21194d2d963e341c5ff390df85c0a15a3
Ilya Levinskii <56347883+DeppaRet@users.noreply.github.com>
2020-10-31 21:42:46 +0300

    modalka

--- 1/WhenINeedToWork/wwwroot/js/index.js
+++ 1/WhenINeedToWork/wwwroot/js/index.js
@@ -1,1 +1,24 @@
-﻿$.modal()
+﻿const modal = $.modal({
+    title: 'Authorization',
+    closable: true,
+    content: `
+    <p>Логин</p> 
+    <input type="email" name="login">
+    <p>Пароль</p> 
+    <input type="password" name = "password">
+    <input type="submit" name="submit" title="Отправить">
+    <p>Text</p>`,
+    width: '700 px',
+    footerButtons: [
+        {
+            text: 'Ok', type: 'primary', handler() {
+                console.log('primary btn clicked')
+            }
+        },
+        {
+            text: 'Cancel', type: 'danger', handler() {
+                console.log('danger btn clicked')
+            }
+        }
+    ]
+})
