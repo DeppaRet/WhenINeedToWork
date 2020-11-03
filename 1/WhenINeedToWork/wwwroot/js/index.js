008039949e4314b74db131bfa9c0414c86ba6e2d
Ilya Levinskii <56347883+DeppaRet@users.noreply.github.com>
2020-11-03 18:31:01 +0300

    макет для расчета ЗП

--- 1/WhenINeedToWork/wwwroot/js/index.js
+++ 1/WhenINeedToWork/wwwroot/js/index.js
@@ -33,12 +33,26 @@
 
 document.addEventListener('click', event => {
     const btnType = event.target.dataset.btn
-    event.preventDefault()
+    //event.preventDefault()                       // блочит все ссылки
 
     if (event.target.dataset.btn === 'login') {
         modal.open()
     }
 });
+
+function calculate() {
+    var calculated = document.getElementById('hours').value * document.getElementById('payment').value;
+    var html;
+    if (document.getElementById('salaryType').value === 'С вычетом налогов') {
+        calculated = calculated * 0.87;
+    }
+    html = 'Рассчитанная ЗП:' + calculated;
+    console.log(calculated);
+    document.getElementById('result').innerHTML = html;
+
+}
+
+document.getElementById('calculateSalary').addEventListener('click', calculate);
 
 function sendInfo() {
     //console.log(login.value)
